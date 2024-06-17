a385b54477e93a5e449afef0b1a782b5487a2ffe
Marlon <marzonhapacheco@gmail.com>
2024-06-17 20:20:29 -0300

    Estilização

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,16 @@ import styles from './Footer.module.css'
 function Footer(props) {
   return (
     <footer className={styles.footer}>
-        <h1>Página Footer</h1>
+        <div className={styles.pfooter}>
+          <a href="">Fale Conosco</a>
+          <a  href="">Suporte</a>
+          <a  href="">Comunidade</a>
+        </div>
+        <div className={styles.pfooter}>
+          <a href="">Parceiros</a>
+          <a href="">Redes Sociais</a>
+          <a href="">Participe</a>
+        </div>
         <p>{props.anoProducao}</p>
     </footer>
   )
